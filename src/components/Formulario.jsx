36b54fa45e8287d53bae54adc3bdd3d090d9e120
CodeBAou <borisafou@gmail.com>
2020-08-19 18:49:39 +0200

    Validacion_Formulario_css_Valid

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -14,8 +14,10 @@ const Formulario = ({SetCita}) => {
        sintomas:''
     });
 
-    const ChangeCitaData= () => {
-        setCita();
+    const ChangeCitaData = e => {
+        setCita(
+            {...cita,[e.target.name]:e.target.value}
+        );
     }
 
     return (
@@ -23,17 +25,17 @@ const Formulario = ({SetCita}) => {
         <form>
             <h2>Añadir Cita</h2>
             <label>DNI</label><br/>
-            <input name="dni" type="text"/><br/>
+            <input name="dni" type="text" pattern="[0-9]{8}[A-Z]{1}" onChange={ChangeCitaData} required/><br/>
             <label>Nombre</label><br/>
-            <input name="nombre" type="text"/><br/>
+            <input name="nombre" type="text" pattern="[A-Za-z ]*" onChange={ChangeCitaData} required/><br/>
             <label>Provincia</label><br/>
-            <input name="provincia" type="text"/><br/>
+            <input name="provincia" type="text" pattern="[A-Za-z]*" onChange={ChangeCitaData} required/><br/>
             <label>Data Cita</label><br/>
-            <input name="data" type="date"/><br/>
+            <input name="data" type="date" onChange={ChangeCitaData} required/><br/>
             <label>Hora</label><br/>
-            <input name="hora" type="time" /><br/>
+            <input name="hora" type="time" onChange={ChangeCitaData} required/><br/>
             <label>Sintomas</label><br/>
-            <textarea name="sintomas" minlength="10"></textarea><br/>
+            <textarea name="sintomas" minLength="10" onChange={ChangeCitaData} required></textarea><br/>
             <input className="SubmitBTN" type="submit" value="Añadir"/>
         </form>
 
