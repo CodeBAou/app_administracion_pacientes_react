4792a4e1f10b8b692b2c476e4f164ba5164a0071
CodeBAou <borisafou@gmail.com>
2020-08-19 14:33:32 +0200

    Se Añade paginacion mobile body

--- src/App.js
+++ src/App.js
@@ -4,18 +4,55 @@ import './assets/css/App.css';
 
 function App() {
 
-   
+    //Pages = añadir/ver
+    const [page,SetPage]=useState('añadir');
+
+    const MobilePageAñadir = () => {
+
+      if(page == 'ver'){
+
+        SetPage('añadir');
+        document.getElementById('BodyCa').style.animation = "DesplazamientoBodyMobile_Right 1s forwards";
+      }
+     
+    }
+
+    const MobilePageVer = () => {
+
+      if(page == 'añadir'){
+
+        SetPage('ver');
+        document.getElementById('BodyCa').style.animation = "DesplazamientoBodyMobile_Left 1s forwards";
+      }
+    }
+
+ 
+
   return (
       
     <div className="content">
-      <div className="titulo_pagina">Administracion pacientes</div>
-      <div className="Header_Api_Pacientes">
+
+      <div className="titulo_pagina">
+
+        <h1>Administracion de Pacientes</h1>
+
+        <div className="MenuMobile">
+          <button className="btnMenuMobile" onClick={MobilePageAñadir}>+</button>
+          <button className="btnMenuMobile" onClick={MobilePageVer}>citas</button>
+        </div>
 
       </div>
 
-      <div className="Api_Pacientes_content">
+      <div id="BodyCa" className="body_Carre">
+          <div id="Page_Añadir" className="Header_Api_Pacientes ">
 
+          </div>
+
+          <div id="Page_Ver" className="Api_Pacientes_content ">
+
+          </div>
       </div>
+
     </div>
 
   );
