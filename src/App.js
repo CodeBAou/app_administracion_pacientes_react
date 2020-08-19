1c926e9cd0eceee8a643b047b553eaa37008ae6e
CodeBAou <borisafou@gmail.com>
2020-08-19 13:03:57 +0200

    css_Header_contenedores_principales

--- src/App.js
+++ src/App.js
@@ -1,45 +1,23 @@
 import React,{useState} from 'react';
-
-
 import PropTypes from 'prop-types';
+import './assets/css/App.css';
 
 function App() {
 
    
-
   return (
-
+      
     <div className="content">
+      <div className="titulo_pagina">Administracion pacientes</div>
+      <div className="Header_Api_Pacientes">
 
-    <div className="Header_Api_Pacientes">
+      </div>
 
-        <h1 className="Title_Page_pacientes">Administracion Citas Pacientes</h1>
+      <div className="Api_Pacientes_content">
 
-        <div className="Select_page">
-
-            <button id="Select_añadir" className="btn-Select_page" onClick={PageVisualizeAñadir}>+</button>
-            <button id="Select_ver" className="btn-Select_page" onClick={PageVisualizeVer}>ver</button>
-
-        </div>
-
+      </div>
     </div>
 
-    <div id="Api_Paciente_content_unic" className="Api_Pacientes_content">
-
-        <div className="Formulario_content">
-
-           
-
-        </div>
-
-        <div className="Citas_content">
-        
-           
-        </div>
-
-    </div>
-
-</div>
   );
 }
 
